9eee897eeda5aeb2f6ac3d6c591fa20b9d0c1bf5
rwitz3 <106227383+rwitz3@users.noreply.github.com>
2022-10-25 16:06:22 +0530

    fresh

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutComponent } from './about/about.component';
+import { LandingPageComponent } from './landing-page/landing-page.component';
+import { SkillsComponent } from './skills/skills.component';
+import { QualificationsComponent } from './qualifications/qualifications.component';
+import { TestimonialsComponent } from './testimonials/testimonials.component';
+import { ContactComponent } from './contact/contact.component';
 
-const routes: Routes = [];
+const routes: Routes = [{path:'about',component:AboutComponent},
+{path:'landing-page',component:LandingPageComponent},
+{path:'skills',component:SkillsComponent},
+{path:'qualifications',component:QualificationsComponent},
+{path:'testimonials',component:TestimonialsComponent},
+{path:'contact',component:ContactComponent}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
+import { FooterComponent } from './footer/footer.component';
+import { HeaderComponent } from './header/header.component';
+import { LandingPageComponent } from './landing-page/landing-page.component';
+import { QualificationsComponent } from './qualifications/qualifications.component';
+import { SkillsComponent } from './skills/skills.component';
+import { TestimonialsComponent } from './testimonials/testimonials.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AboutComponent,
+    ContactComponent,
+    FooterComponent,
+    HeaderComponent,
+    LandingPageComponent,
+    QualificationsComponent,
+    SkillsComponent,
+    TestimonialsComponent
   ],
   imports: [
     BrowserModule,
